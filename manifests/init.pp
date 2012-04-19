c309705f6f062fb1b63d7ef6274fe633fc72a90c
Andreas Paul <xorpaul@gmail.com>
2012-04-19 16:51:48 +0200

    tried using another class with require

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,23 @@
 class example {
+
   define ressource {
     notify { "$name": }
 
-    file { "/tmp/all-names.xml-${name}":
-            content => template("example/all-ressources.erb"),
-    }
+    #file { "/tmp/all-names.xml-${name}":
+    #  content => template("example/all-ressources.erb"),
+    #}
 
   }
 }
 
 example::ressource { "test1": }
 example::ressource { "test2": }
+require example::once
+
+class example::once {
+
+    file { "/tmp/all-names.xml":
+      content => template("example/all-ressources.erb"),
+    }
+
+}
